1b8521e70eac2ee86eb7a00498f43061ff0b0e1e
Krabs <vovastr0v@yandex.ru>
2024-12-20 04:39:51 +0500

    FIX

--- backend/settings.py
+++ backend/settings.py
@@ -220,6 +220,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 ALLOWED_HOSTS = ["*"]
